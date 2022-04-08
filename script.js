12c10db2f2d3ab4be82dbb0b9570257820231dba
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2022-04-08 10:51:58 +0200

    Store incoming message in localStorage

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ class Notification {
 }
 
 const createNot = new Notification() //You have 0 new notification 
-const create2Not = new Notification('', 'New offers!!, Hurry GET it Now!!!', new Date().toDateString()) //You have 1 new notification 
+const create2Not = new Notification({mType: '', message: 'New offers!!, Hurry GET it Now!!!', date: new Date().toDateString()}) //You have 1 new notification 
 
 //Set the 3 object's value for createNot and create2Not.mType 
 createNot.mType = 'notification'
@@ -53,13 +53,22 @@ const getNot = (incomingNot) => {
         let combine = function(msg) {
             if(msg) {
               createCard.cad.innerHTML.replace = icon.setAttribute('data-tooltip',
-              newMsg  + ' ' +create2Not.mType+ ' ' + createNot.date+ ' ' +` Messag: ${createNot.message}`) 
+              newMsg + ' ' +create2Not.mType+ ' ' + createNot.date+ ' ' +` Messag: ${createNot.message}`) 
             }
           };
 
         //Check if incomingNot is undefined and return default text and 
         //if it is defined it means we have new message then return the message
         if ( incomingNot !== undefined ) incomingNot ? combine(incomingNot) : defaultNot;
+
+        // let not = combine(incomingNot);
+        let notMsg = localStorage.setItem(`${newMsg}`, `${createNot.message}`)
+        let notObj = []
+        const newNot = localStorage.getItem(notMsg)
+        notObj.push(newNot)
+        
+          console.log(notObj)
+          
 
             specCard.textContent = newMsg 
             specCard.style.color = 'red'
